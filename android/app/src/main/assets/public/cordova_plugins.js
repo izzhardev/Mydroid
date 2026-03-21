3298fd4df4d478b9a367a18f92f5de15d6159e64
izzhardev <njas.izzhar@gmail.com>
2026-03-22 02:12:59 +0700

    Fix: Force upload android folder and gradlew permissions

---
+++ android/app/src/main/assets/public/cordova_plugins.js
@@ -0,0 +1,20 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "cordova-plugin-httpd.CorHttpd",
+          "file": "plugins/cordova-plugin-httpd/www/CorHttpd.js",
+          "pluginId": "cordova-plugin-httpd",
+        "clobbers": [
+          "cordova.plugins.CorHttpd"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "cordova-plugin-httpd": "0.9.3"
+    };
+    // BOTTOM OF METADATA
+    });
+    
